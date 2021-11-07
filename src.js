743e6e4b94b226fedb1c5046604828779ebdd23b
linzjane <87429934+linzjane@users.noreply.github.com>
2021-11-07 15:09:48 +0000

    Added JS for remove buttons

--- src.js
+++ src.js
@@ -6,11 +6,24 @@ let addButtonC = document.querySelector("#add-button-C");
 addButtonC.addEventListener("click", addItemC);
 let addButtonD = document.querySelector("#add-button-D");
 addButtonD.addEventListener("click", addItemD);
+let removeButtonA = document.querySelector("#remove-button-A");
+removeButtonA.addEventListener("click", removeItemA);
+let removeButtonB = document.querySelector("#remove-button-B");
+removeButtonB.addEventListener("click", removeItemB);
+let removeButtonC = document.querySelector("#remove-button-C");
+removeButtonC.addEventListener("click", removeItemC);
+let removeButtonD = document.querySelector("#remove-button-D");
+removeButtonD.addEventListener("click", removeItemD);
 
 var a = document.getElementById("quantityA").innerText;
 function addItemA(event) {
   event.preventDefault();
   a++;
+  document.getElementById("quantityA").innerText = a;
+}
+function removeItemA(event) {
+  event.preventDefault();
+  a--;
   document.getElementById("quantityA").innerText = a;
 }
 var b = document.getElementById("quantityB").innerText;
@@ -19,15 +32,30 @@ function addItemB(event) {
   b++;
   document.getElementById("quantityB").innerText = b;
 }
+function removeItemB(event) {
+  event.preventDefault();
+  b--;
+  document.getElementById("quantityB").innerText = b;
+}
 var c = document.getElementById("quantityC").innerText;
 function addItemC(event) {
   event.preventDefault();
   c++;
   document.getElementById("quantityC").innerText = c;
 }
+function removeItemC(event) {
+  event.preventDefault();
+  c--;
+  document.getElementById("quantityC").innerText = c;
+}
 var d = document.getElementById("quantityD").innerText;
 function addItemD(event) {
   event.preventDefault();
   d++;
+  document.getElementById("quantityD").innerText = d;
+}
+function removeItemD(event) {
+  event.preventDefault();
+  d--;
   document.getElementById("quantityD").innerText = d;
 }
